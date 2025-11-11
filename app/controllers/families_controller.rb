07910ca64198e2475adc357ca8ab851ee93afa23
Gregory Lefebvre <gregory.lfb1@gmail.com>
2025-11-11 15:14:31 +0100

    family crud finished

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,50 @@
 class FamiliesController < ApplicationController
+  before_action :set_family, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @families = Family.all
+  end
+
+  def show
+  end
+
+  def new
+    @families = Family.new
+  end
+
+  def create
+    @family = Family.new(family_params)
+    if @family.save
+      redirect_to @family, notice: "Family created succesfully ! Welcome !"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @family = Family.new(family_params)
+  end
+
+  def update
+    if @family.update(family_params)
+      redirect_to @family, notice: "Successfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to families_path, notice: "Family deleted."
+  end
+
+  private
+
+  def set_family
+    @family = Family.find(params[:id])
+  end
+
+  def family_params
+    params.require(:family).permit(:name, :zipcode, :user)
+  end
 end
