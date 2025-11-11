69267764bb3006fdf70ea49f8f72f880cfd3d2d4
goze kacmaz <gozekacmaz@gmail.com>
2025-11-11 17:03:13 +0100

    les routes revues et validées par dylane

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,18 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :family do
+    resources :members, only: [:index, :new, :create]
+    resources :chats, only: [:index, :create]
+  end
+
+  resources :members, only: [:show, :edit, :update, :destroy]
+
+  resources :chats, only: :show do
+    resources :messages, only: :create
+  end
+
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
