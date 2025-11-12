c6fbaa653b1191b17e2e8b3d326d4e63ed5e5eb4
goze kacmaz <gozekacmaz@gmail.com>
2025-11-12 15:49:02 +0100

    prompt v.0

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,5 +2,5 @@ class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :family
   has_many :messages, dependent: :destroy
-  SYSTEM_PROMPT = ""
+  SYSTEM_PROMPT = "you are a family assistant. you need to create a calendar that will include family's birthday's and vaccines. Context : use the age of family members and their zipcode. You will present the informations the following way : 1/ family holidays 2/ family members' vaccines."
 end
